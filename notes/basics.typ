603c83bafefb7029440712a32b3c95f5758f24ff
jsacob <jacobmofficial@outlook.com>
2025-09-28 23:09:57 -0400

    shadingNeeded

--- notes/basics.typ
+++ notes/basics.typ
@@ -1,2 +1,13 @@
 = OpenGL Data types 
 https://www.khronos.org/opengl/wiki/OpenGL_Type
+
+Points refer to vertices (A vertex has 3 coordinates: X, Y and Z)
+X is right
+Y is up
+Z is towards your back (behind)
+
+(-1,-1) is the bottom left of the screen
+(1,-1) is the bottom right of the screen
+(0,1) is the middle top.
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,23 @@ int main()
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
+	
+	static const GLfloat g_vertex_buffer_data[] = {
+	-1.0f, -1.0f, 0.0f,
+	1.0f, -1.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+	};
 
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDisableVertexAttribArray(0);
 	}
 	glfwTerminate();
 	return 0;
